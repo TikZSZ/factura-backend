948ee48c4cba5869385315f454f545faf4f95c62
TikZSZ <repackergame@gmail.com>
2021-12-10 11:08:01 +0530

    fix migration file

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', true);
   app.enableCors({
     credentials: true,
-    methods: '*',
+    methods: ['GET', 'PUT', 'POST'],
     exposedHeaders: 'set-cookie',
     origin:
       process.env.NODE_ENV === 'production'
         ? 'https://factura-frontend.vercel.app'
         : 'http://localhost:3000',
   });
-  app.set('trust proxy', true);
   app.useGlobalPipes(new ValidationPipe());
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);

1ef8a1fb95079eabe9226d7103f46b1ec85ce934
TikZSZ <repackergame@gmail.com>
2021-12-10 11:01:01 +0530

    fix migration file

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
-import {NestExpressApplication} from "@nestjs/platform-express"
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    credentials:true,
-    origin:process.env.NODE_ENV === "production"?'https://factura-frontend.vercel.app':'http://localhost:3000',
-  })
-  app.set('trust proxy',true)
+    credentials: true,
+    methods: '*',
+    exposedHeaders: 'set-cookie',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://factura-frontend.vercel.app'
+        : 'http://localhost:3000',
+  });
+  app.set('trust proxy', true);
   app.useGlobalPipes(new ValidationPipe());
   const prismaService: PrismaService = app.get(PrismaService);
-  prismaService.enableShutdownHooks(app)
+  prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
